e7832183a994d8eab7561632c6ad3803b6ad9931
leorambo <leorambo@163.com>
2020-12-08 14:39:38 +0800

    updating...

--- src/app/admin/product-create/product-create.component.ts
+++ src/app/admin/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { AlertService } from './../../alert/alert.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ProductService } from "../product.service";
@@ -15,7 +16,8 @@ export class ProductCreateComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private service: ProductService
+    private service: ProductService,
+    private alertService : AlertService
   ) {
     this.myForm = new FormGroup({
       imgUrl: new FormControl('', [Validators.required]),
@@ -30,9 +32,19 @@ export class ProductCreateComponent implements OnInit {
   onSubmit() {
     if (this.myForm.valid) {
       this.service.createProduct(this.myForm.value).subscribe(data => {
-        // 此处应该返回到商品列表页面
-        this.router.navigate(['/admin/product-list']);
 
+        if (data['code'] == '0') {
+          
+          // 注册成功即为登录成功
+          this.alertService.success('商品添加成功！正在跳转...');
+          setTimeout(() => {
+            // 此处应该返回到商品列表页面
+            this.router.navigate(['/admin/product-list']);
+          }, 3000);
+        }
+        else {
+          this.alertService.error(data['msg']);
+        }
       });
     }
   }
